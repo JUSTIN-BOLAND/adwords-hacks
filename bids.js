41defa9bf9a684cb6d73358e729cf6e7edb4cb0f
Sayem Khan <sayemmkhan@gmail.com>
2016-11-16 01:22:39 -0500

    change bids based on avg position

---
+++ bids.js
@@ -0,0 +1,48 @@
+function main() {
+  var campaignIterator = AdWordsApp.campaigns().get();
+
+  while (campaignIterator.hasNext()) {
+    var campaign = campaignIterator.next();
+    var allTimeStats = campaign.getStatsFor('ALL_TIME');
+    var spend = allTimeStats.getCost();
+
+    if (spend > 125 && campaign.isEnabled()) {
+      var todayStats = campaign.getStatsFor('TODAY');
+      var position = todayStats.getAveragePosition();
+
+      var adGroupSelector = AdWordsApp.adGroups();
+      var adGroupIterator = adGroupSelector.get();
+
+      while (adGroupIterator.hasNext()) {
+        var adGroup = adGroupIterator.next();
+        var currentBid = adGroup.bidding().getCpc();
+
+        if (adGroup.isEnabled()) {
+          if (position < 2) {
+            // lower by 0.25
+            adGroup.bidding().setCpc(currentBid - 0.25);
+          } else if (position >= 2 && position < 3) {
+            // max bid 1.5
+            if (currentBid > 1.5) {
+              adGroup.bidding().setCpc(1.5);
+            }
+          } else if (position >= 3.5 && position < 4) {
+            // max bid 1.5 and increase by 0.25
+            if (currentBid > 1.5) {
+              adGroup.bidding().setCpc(1.5);
+            } else {
+              adGroup.bidding().setCpc(currentBid + 0.25);
+            }
+          } else if (position >= 4) {
+            // max bid 1.75 and increase by 0.5
+            if (currentBid > 1.75) {
+              adGroup.bidding().setCpc(1.75);
+            } else {
+              adGroup.bidding().setCpc(currentBid + 0.5);
+            }
+          }
+        }
+      }
+    }
+  }
+}
