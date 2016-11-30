bb19b5ca63c776be31870ea4625e3e7f3dc3f37a
Sayem Khan <sayemmkhan@gmail.com>
2016-11-29 23:38:37 -0500

    fixed

--- bids.js
+++ bids.js
@@ -10,36 +10,31 @@ function main() {
       var todayStats = campaign.getStatsFor('TODAY');
       var position = todayStats.getAveragePosition();
 
-      var adGroupSelector = AdWordsApp.adGroups();
-      var adGroupIterator = adGroupSelector.get();
+      var adGroup = AdWordsApp.adGroups().withCondition('Name = "Ad Group #2"').get().next();
+      var currentBid = adGroup.bidding().getCpc();
 
-      while (adGroupIterator.hasNext()) {
-        var adGroup = adGroupIterator.next();
-        var currentBid = adGroup.bidding().getCpc();
-
-        if (adGroup.isEnabled()) {
-          if (position < 2) {
-            // lower by 0.25
-            adGroup.bidding().setCpc(currentBid - 0.25);
-          } else if (position >= 2 && position < 3) {
-            // max bid 1.5
-            if (currentBid > 1.5) {
-              adGroup.bidding().setCpc(1.5);
-            }
-          } else if (position >= 3.5 && position < 4) {
-            // max bid 1.5 and increase by 0.25
-            if (currentBid > 1.5) {
-              adGroup.bidding().setCpc(1.5);
-            } else {
-              adGroup.bidding().setCpc(currentBid + 0.25);
-            }
-          } else if (position >= 4) {
-            // max bid 1.75 and increase by 0.5
-            if (currentBid > 1.75) {
-              adGroup.bidding().setCpc(1.75);
-            } else {
-              adGroup.bidding().setCpc(currentBid + 0.5);
-            }
+      if (adGroup.isEnabled()) {
+        if (position > 0 && position < 2) {
+          // lower by 0.25
+          adGroup.bidding().setCpc(currentBid - 0.25);
+        } else if (position >= 2 && position < 3) {
+          // max bid 1.5
+          if (currentBid > 1.5) {
+            adGroup.bidding().setCpc(1.5);
+          }
+        } else if (position >= 3.5 && position < 4) {
+          // max bid 1.5 and increase by 0.25
+          if (currentBid > 1.5) {
+            adGroup.bidding().setCpc(1.5);
+          } else {
+            adGroup.bidding().setCpc(currentBid + 0.25);
+          }
+        } else if (position >= 4) {
+          // max bid 1.75 and increase by 0.5
+          if (currentBid > 1.75) {
+            adGroup.bidding().setCpc(1.75);
+          } else {
+            adGroup.bidding().setCpc(currentBid + 0.5);
           }
         }
       }
